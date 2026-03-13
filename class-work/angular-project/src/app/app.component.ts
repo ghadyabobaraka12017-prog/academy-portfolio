a020172c248cff89f5de96b980f74cdf5f60b310
Ghadi Bou Barakeh <20221042@ua.edu.lb>
2026-03-13 14:25:46 +0200

    create the navbar and profile component

---
+++ class-work/angular-project/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {ProfileComponent} from './components/profile/profile.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-project';
+}
