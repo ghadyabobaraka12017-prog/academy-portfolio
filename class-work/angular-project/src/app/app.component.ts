dc986f7ccb8e4d79f0d752fed8437bd322038f98
Ghadi Bou Barakeh <20221042@ua.edu.lb>
2026-03-13 21:39:36 +0200

    add the projects

--- class-work/angular-project/src/app/app.component.ts
+++ class-work/angular-project/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {ProfileComponent} from './components/profile/profile.component';
 import {AboutComponent} from './components/about/about.component';
+import {ProjectsComponent} from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ProfileComponent, AboutComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
