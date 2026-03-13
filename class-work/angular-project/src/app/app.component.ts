e9dd4e7e035080d1c95ad29a3836da9c46873673
Ghadi Bou Barakeh <20221042@ua.edu.lb>
2026-03-13 20:11:15 +0200

    add the about section and fix the styles.scss

--- class-work/angular-project/src/app/app.component.ts
+++ class-work/angular-project/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {ProfileComponent} from './components/profile/profile.component';
+import {AboutComponent} from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
